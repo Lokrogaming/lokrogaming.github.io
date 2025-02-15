4f0bb614e69bccb337f76c23929db9d6def6bcdd
Lokrogamer <lokrogamer.29@gmail.com>
2025-02-16 00:49:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function opengame() {
+  window.location.href ="guesser2.html"
+}
+function opentime() {
+  window.location.href ="time3.html"
+}
+function backtomenu() {
+  window.location.href ="game-library.html"
+}
+function openlibrary() {
+  window.location.href="game-library.html"
+}
+function openbot() {
+  window.location.href="discord-bot.html"
+}
+function opendownloader() {
+  window.location.href="download-hub.html"
+}
+function downloadtimer() {
+  window.location.href="time.zip"
+}
+function downloadLogo() {
+  window.location.href="logo.zip"
+}
+function downloadbewerbung() {
+  window.location.href="Bewerbung-Content_creator.zip"
+}
+function downloadmemes() {
+  window.location.href="memes.zip"
+}
+function opentwitchplayer() {
+  window.location.href="musicplayer.html"
+}
+function downloadmsgs() {
+  window.location.href="msg-coded.zip"
+}
+function opendiscord() {
+  window.open("Discord.html")
+}
+function opentest() {
+  window.location.href="test-coupon-page.html"
+}
