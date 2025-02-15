c01130a4a81c05ddb6ee7d09b2889666a8d565bc
Lokrogamer <lokrogamer.29@gmail.com>
2025-02-16 00:54:55 +0100

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -29,7 +29,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("democount").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
@@ -52,6 +52,7 @@ let btnpress = localstorage.getItem(checkdcbtnpress);
   else {
     window.alert("You can't click the button twice!");
   }
+}
     
     
   
